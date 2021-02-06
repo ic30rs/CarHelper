f81e28e5db839ed6d656fd26ac2e84a59c9994de
ic30rs <ic30rs@gmail.com>
2021-02-06 18:55:27 +0800

    DOGGIE

--- src/business/check/GeneralMessageChecker.java
+++ src/business/check/GeneralMessageChecker.java
@@ -1,5 +1,6 @@
 package business.check;
 
+import business.model.EngineData;
 import business.model.MessageBean;
 import business.model.TotalCarData;
 
@@ -21,7 +22,7 @@ public class GeneralMessageChecker extends AbstractMessageChecker{
         boolean isLingZhi = false;
         boolean isFanWeiYiChang = false;
 
-        //判断空值的方法
+        //判断整车数据
         if(bean.getTotalCarData() == null){
             isKongZhi = true;
         }else{
@@ -29,26 +30,203 @@ public class GeneralMessageChecker extends AbstractMessageChecker{
             int carStatus = bean.getTotalCarData().getCarStatus();
             if(carStatus == 0xFE) isYiChang = true;
             if(carStatus == 0xFF) isWuXiao = true;
-            //------------------------------
+
+            //---充电状态---
             int chargeStatus = bean.getTotalCarData().getChargeStatus();
             if(chargeStatus == 0xFE) isYiChang = true;
             if(chargeStatus == 0xFF) isWuXiao = true;
 
-            if(carStatus != 0x02){//不是停车状态
-                TotalCarData totalCarData = bean.getTotalCarData();
-                float speed = totalCarData.getVelocity();
-                //判断范围异常
-                if(speed < 0 || speed > 220) isFanWeiYiChang = true;
-                //判断零值
-                if(speed == 0) isLingZhi = true;
-                //对于之前转换过的小数，我们把FE FF按照一样的方法转换，判断是否相等
-                if(speed == Integer.parseInt("FFFE", 16) * 0.1f) isYiChang = true;
-                if(speed == Integer.parseInt("FFFF", 16) * 0.1f) isWuXiao = true;
+            //---运行状态---
+            int runStatus = bean.getTotalCarData().getRunStatus();
+            if(runStatus == 0xFE) isYiChang = true;
+            if(runStatus == 0xFF) isWuXiao = true;
 
-            }else{//是停车状态
+            //---车速---
+            TotalCarData totalCarData = bean.getTotalCarData();
+            float speed = totalCarData.getVelocity();
+            if(speed < 0 || speed > 220) isFanWeiYiChang = true;
+            if(speed == 0) isLingZhi = true;
+            if(speed == Integer.parseInt("FFFE", 16) * 0.1f) isYiChang = true;
+            if(speed == Integer.parseInt("FFFF", 16) * 0.1f) isWuXiao = true;
 
-            }
+            //---累计里程---
+            float totalDistance = totalCarData.getTotalDistance();
+            if(totalDistance < 0 || totalDistance > 9999999) isFanWeiYiChang = true;
+            if(speed == 0) isLingZhi = true;
+            if(speed == Integer.parseInt("FFFFFFFE", 16) * 0.1f) isYiChang = true;
+            if(speed == Integer.parseInt("FFFFFFFF", 16) * 0.1f) isWuXiao = true;
+
+            //---总电压---
+            float zongdianya = totalCarData.getZongDianYa();
+            if(zongdianya < 0 || zongdianya > 10000) isFanWeiYiChang = true;
+            if(zongdianya == 0) isLingZhi = true;
+            if(zongdianya == Integer.parseInt("FFFFFFFE", 16) * 0.1f) isYiChang = true;
+            if(zongdianya == Integer.parseInt("FFFFFFFF", 16) * 0.1f) isWuXiao = true;
+
+            //---总电流---
+            float zongdianliu = totalCarData.getZongDianLiu();
+            if(zongdianliu < -1000 || zongdianliu > 1000) isFanWeiYiChang = true;
+            if(zongdianliu == 0) isLingZhi = true;
+            if(zongdianliu == Integer.parseInt("FFFE", 16) * 0.1f) isYiChang = true;
+            if(zongdianliu == Integer.parseInt("FFFF", 16) * 0.1f) isWuXiao = true;
+
+            //---SOC---
+            int soc = bean.getTotalCarData().getSOC();
+            if(soc < 0 || soc > 100) isFanWeiYiChang = true;
+            if(soc == 0) isLingZhi = true;
+            if(soc == 0xFE) isYiChang = true;
+            if(soc == 0xFF) isWuXiao = true;
+
+            //---SOC---
+            int dcdc = bean.getTotalCarData().getDC_DC_Status();
+            if(dcdc == 0) isLingZhi = true;
+            if(dcdc == 0xFE) isYiChang = true;
+            if(dcdc == 0xFF) isWuXiao = true;
+
         }
+
+        //判断驱动电机数据
+        if(bean.getDianJiDataList() == null){
+            isKongZhi = true;
+        }else{
+
+        }
+
+        //判断发动机数据
+        if(bean.getEngineData() == null){
+            isKongZhi = true;
+        }else{
+
+            //---发动机状态---
+            int engineStatus = bean.getEngineData().getEngineStatus();
+            if(engineStatus == 0) isLingZhi = true;
+            if(engineStatus == 0xFE) isYiChang = true;
+            if(engineStatus == 0xFF) isWuXiao = true;
+
+            //---曲轴转速---
+            int axleSpeed = bean.getEngineData().getAxleSpeed();
+            if(axleSpeed == 0) isLingZhi = true;
+            if(axleSpeed == 0xFFFE) isYiChang = true;
+            if(axleSpeed == 0xFFFF) isWuXiao = true;
+
+            //---燃料燃烧率---
+            EngineData engineData = bean.getEngineData();
+            float fuelRatio = engineData.getFuelRatio();
+            if(fuelRatio < 0 || fuelRatio > 60000) isFanWeiYiChang = true;
+            if(fuelRatio == 0) isLingZhi = true;
+            if(fuelRatio == Integer.parseInt("FFFE", 16) * 0.01f) isYiChang = true;
+            if(fuelRatio == Integer.parseInt("FFFF", 16) * 0.01f) isWuXiao = true;
+
+
+        }
+
+        //判断车辆位置
+        if(bean.getCarPosition() == null){
+            isKongZhi = true;
+        }else{
+
+            //---定位状态---
+            int positionStatus = bean.getCarPosition().getPositionStatus();
+            if(positionStatus == 0x01) isYiChang = true;
+
+
+        }
+
+        //判断极值数据
+        if(bean.getExtremeData() == null){
+            isKongZhi = true;
+        }else{
+
+            //---最高电压系统号---
+            int highBatterySysNum = bean.getExtremeData().getHighBatterySysNum();
+            if(highBatterySysNum < 0 || highBatterySysNum > 250) isFanWeiYiChang = true;
+            if(highBatterySysNum == 0) isLingZhi = true;
+            if(highBatterySysNum == 0xFE) isYiChang = true;
+            if(highBatterySysNum == 0xFF) isWuXiao = true;
+
+            //---最高单体电压号---
+            int highBatteryNum = bean.getExtremeData().getHighBatteryNum();
+            if(highBatteryNum < 0 || highBatteryNum > 250) isFanWeiYiChang = true;
+            if(highBatteryNum == 0) isLingZhi = true;
+            if(highBatteryNum == 0xFE) isYiChang = true;
+            if(highBatteryNum == 0xFF) isWuXiao = true;
+
+            //---电池单体电压最高值---
+            float highBattery = bean.getExtremeData().getHighBattery();
+            if(highBattery < 0 || highBattery > 15) isFanWeiYiChang = true;
+            if(highBattery == 0) isLingZhi = true;
+            if(highBattery == Integer.parseInt("FFFE", 16) * 0.001f) isYiChang = true;
+            if(highBattery == Integer.parseInt("FFFF", 16) * 0.001f) isWuXiao = true;
+
+            //---最低电压系统号---
+            int lowBatterySysNum = bean.getExtremeData().getLowBatterySysNum();
+            if(lowBatterySysNum < 0 || lowBatterySysNum > 250) isFanWeiYiChang = true;
+            if(lowBatterySysNum == 0) isLingZhi = true;
+            if(lowBatterySysNum == 0xFE) isYiChang = true;
+            if(lowBatterySysNum == 0xFF) isWuXiao = true;
+
+            //---最低单体电压号---
+            int lowBatteryNum = bean.getExtremeData().getLowBatteryNum();
+            if(lowBatteryNum < 0 || lowBatteryNum > 250) isFanWeiYiChang = true;
+            if(lowBatteryNum == 0) isLingZhi = true;
+            if(lowBatteryNum == 0xFE) isYiChang = true;
+            if(lowBatteryNum == 0xFF) isWuXiao = true;
+
+            //---电池单体电压最低值---
+            float lowBattery = bean.getExtremeData().getLowBattery();
+            if(lowBattery < 0 || lowBattery > 15) isFanWeiYiChang = true;
+            if(lowBattery == 0) isLingZhi = true;
+            if(lowBattery == Integer.parseInt("FFFE", 16) * 0.001f) isYiChang = true;
+            if(lowBattery == Integer.parseInt("FFFF", 16) * 0.001f) isWuXiao = true;
+
+            //---最高温度子系统---
+            int highSysNum = bean.getExtremeData().getHighSysNum();
+            if(highSysNum < 0 || highSysNum > 250) isFanWeiYiChang = true;
+            if(highSysNum == 0) isLingZhi = true;
+            if(highSysNum == 0xFE) isYiChang = true;
+            if(highSysNum == 0xFF) isWuXiao = true;
+
+            //---最高温度探针号---
+            int highProbeNum = bean.getExtremeData().getHighProbeNum();
+            if(highProbeNum < 0 || highProbeNum > 250) isFanWeiYiChang = true;
+            if(highProbeNum == 0) isLingZhi = true;
+            if(highProbeNum == 0xFE) isYiChang = true;
+            if(highProbeNum == 0xFF) isWuXiao = true;
+
+            //---最高温度值---
+            int highTemp = bean.getExtremeData().getHighTemp();
+            if(highTemp < -40 || highTemp > 210) isFanWeiYiChang = true;
+            if(highTemp == 0) isLingZhi = true;
+            if(highTemp == 0xFE) isYiChang = true;
+            if(highTemp == 0xFF) isWuXiao = true;
+
+            //---最低温度子系统---
+            int lowSysNum = bean.getExtremeData().getLowSysNum();
+            if(lowSysNum < 0 || highSysNum > 250) isFanWeiYiChang = true;
+            if(lowSysNum == 0) isLingZhi = true;
+            if(lowSysNum == 0xFE) isYiChang = true;
+            if(lowSysNum == 0xFF) isWuXiao = true;
+
+            //---最低温度探针号---
+            int lowProbeNum = bean.getExtremeData().getLowProbeNum();
+            if(lowProbeNum < 0 || highProbeNum > 250) isFanWeiYiChang = true;
+            if(lowProbeNum == 0) isLingZhi = true;
+            if(lowProbeNum == 0xFE) isYiChang = true;
+            if(lowProbeNum == 0xFF) isWuXiao = true;
+
+            //---最低温度值---
+            int lowTemp = bean.getExtremeData().getLowTemp();
+            if(lowTemp < -40 || lowTemp > 210) isFanWeiYiChang = true;
+            if(lowTemp == 0) isLingZhi = true;
+            if(lowTemp == 0xFE) isYiChang = true;
+            if(lowTemp == 0xFF) isWuXiao = true;
+
+
+
+
+        }
+
+
 
 
 
